96e15012cf2855d254c9bef3532a7f249f360ba5
Yasunori Fujie <fuji@dmgw.net>
2020-08-19 00:35:25 +0900

    Add azure functions adaptor

--- all/init.go
+++ all/init.go
@@ -2,4 +2,5 @@ package all
 
 import (
 	_ "github.com/yacchi/lambda-http-adaptor/aws"
+	_ "github.com/yacchi/lambda-http-adaptor/azure"
 )
---
+++ azure/functions.go
@@ -0,0 +1,44 @@
+package azure
+
+import (
+	"context"
+	"github.com/yacchi/lambda-http-adaptor/registry"
+	"github.com/yacchi/lambda-http-adaptor/types"
+	"net/http"
+	"os"
+)
+
+const (
+	FunctionsHTTPWorkerPortEnvKey = "FUNCTIONS_HTTPWORKER_PORT"
+)
+
+type FunctionsAdaptor struct {
+	s *http.Server
+}
+
+func (f FunctionsAdaptor) ListenAndServe() error {
+	return f.s.ListenAndServe()
+}
+
+func (f FunctionsAdaptor) Shutdown(ctx context.Context) error {
+	return f.s.Shutdown(ctx)
+}
+
+func FunctionsDetector() bool {
+	_, ok := os.LookupEnv(FunctionsHTTPWorkerPortEnvKey)
+	return ok
+}
+
+func NewAzureFunctionsAdaptor(addr string, h http.Handler) types.Adaptor {
+	port := os.Getenv(FunctionsHTTPWorkerPortEnvKey)
+	return &FunctionsAdaptor{
+		s: &http.Server{
+			Addr:    ":" + port,
+			Handler: h,
+		},
+	}
+}
+
+func init() {
+	registry.Registry.AddAdaptor("azure_functions", FunctionsDetector, NewAzureFunctionsAdaptor)
+}
